935e487e75e7c4aee4e0d429b7bcf474550d25a1
Eli Mallon <eli@stream.place>
2017-10-30 17:17:42 -0700

    🎈

---
+++ main.js
@@ -0,0 +1,62 @@
+const { BrowserWindow, app, globalShortcut } = require('electron')
+
+if (!process.argv[2]) {
+  console.error(`Usage: floating-hangout [https://hangouts.google.com/path/to/hangout]`);
+  process.exit(1);
+}
+
+const hangoutUrl = process.argv[2];
+
+app.on("ready", () => {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    transparent: true,
+    // frame: false,
+    // opacity:0.5,
+    alwaysOnTop: true,
+    titleBarStyle: "hidden-inset"
+  })
+  win.show()
+
+  win.on('closed', () => {
+    win = null
+  })
+
+  console.log(process.argv)
+
+  // Load a remote URL
+  win.loadURL('https://hangouts.google.com/hangouts/_/i6gnba6x4vb3dkt3vtmbduppo4e?hl=en');
+
+  const show = () => {
+    win.webContents.executeJavaScript(`
+      document.body.style.opacity = 1;
+      // document.body.style.backgroundColor = "transparent";
+      document.body.style.webkitAppRegion = "drag";
+    `);
+    win.setIgnoreMouseEvents(false);
+  }
+
+  const hide = () => {
+    win.webContents.executeJavaScript(`
+      document.body.style.opacity = 0.75;
+      // document.body.style.backgroundColor = "transparent";
+    `);
+    win.setIgnoreMouseEvents(true);
+  }
+
+  let ghosted = false;
+  globalShortcut.register("Shift+Ctrl+Option+Command+F", () => {
+    if (ghosted) {
+      show();
+    }
+    else {
+      hide();
+    }
+    ghosted = !ghosted;
+  });
+
+  win.webContents.on('dom-ready', () => {
+    show();
+  });
+});
